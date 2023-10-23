63976543b10da889f8ff9077fc6ae9ba1a9cf7dc
OttJoh94 <olg.johansson@gmail.com>
2023-10-23 22:20:32 +0200

    TravelDetailsWindow done

--- TravelPal/Windows/TravelDetailsWindow.xaml.cs
+++ TravelPal/Windows/TravelDetailsWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using TravelPal.Managers;
 using TravelPal.Models;
@@ -17,26 +18,33 @@ namespace TravelPal.Windows
             cbReason.Items.Add("Work trip");
             cbReason.Items.Add("Vacation");
 
+            UpdateUI();
 
-            txtDestination.Text = TravelManager.SelectedTravel.Destination;
-            txtTravellers.Text = TravelManager.SelectedTravel.Travellers.ToString();
+        }
+
+        private void UpdateUI()
+        {
+            Travel selectedTravel = TravelManager.SelectedTravel;
+
+            txtDestination.Text = selectedTravel.Destination;
+            txtTravellers.Text = selectedTravel.Travellers.ToString();
             foreach (Country country in Enum.GetValues(typeof(Country)))
             {
                 cbCountry.Items.Add(country);
             }
-            cbCountry.SelectedItem = TravelManager.SelectedTravel.Country;
-            dateStartDate.SelectedDate = TravelManager.SelectedTravel.StartDate;
-            dateEndDate.SelectedDate = TravelManager.SelectedTravel.EndDate;
-            txtDaysOfTravelling.Text = TravelManager.SelectedTravel.TravelDays.ToString();
+            cbCountry.SelectedItem = selectedTravel.Country;
+            dateStartDate.SelectedDate = selectedTravel.StartDate;
+            dateEndDate.SelectedDate = selectedTravel.EndDate;
+            txtDaysOfTravelling.Text = selectedTravel.TravelDays.ToString();
 
-            foreach (PackingListItem item in TravelManager.SelectedTravel.PackingList)
+            foreach (PackingListItem item in selectedTravel.PackingList)
             {
                 lstPackingList.Items.Add(item);
             }
 
-            if (TravelManager.SelectedTravel.GetType() == typeof(WorkTrip))
+            if (selectedTravel.GetType() == typeof(WorkTrip))
             {
-                WorkTrip workTrip = (WorkTrip)TravelManager.SelectedTravel;
+                WorkTrip workTrip = (WorkTrip)selectedTravel;
                 //Enable meeting details
                 cbReason.SelectedItem = "Work trip";
                 lblMeetingDetails.Visibility = Visibility.Visible;
@@ -46,7 +54,7 @@ namespace TravelPal.Windows
             }
             else
             {
-                Vacation vacation = (Vacation)TravelManager.SelectedTravel;
+                Vacation vacation = (Vacation)selectedTravel;
                 //Välj vacation i cbReason
                 //Enable AllInclusive
                 cbReason.SelectedItem = "Vacation";
@@ -57,11 +65,6 @@ namespace TravelPal.Windows
                     xbAllInclusive.IsChecked = true;
                 }
             }
-
-
-
-
-
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
@@ -69,6 +72,114 @@ namespace TravelPal.Windows
             TravelsWindow travelsWindow = new();
             travelsWindow.Show();
             Close();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+            TurnOnUI();
+
+        }
+
+        private void TurnOnUI()
+        {
+            //Byt knapp
+            btnEdit.Visibility = Visibility.Hidden;
+            btnSave.Visibility = Visibility.Visible;
+
+            //Gör så man kan ändra i rutorna
+            txtDestination.IsReadOnly = false;
+            cbCountry.IsEnabled = true;
+            txtTravellers.IsReadOnly = false;
+            cbReason.IsEnabled = true;
+            txtMeetingDetails.IsReadOnly = false;
+            xbAllInclusive.IsEnabled = true;
+            dateStartDate.IsEnabled = true;
+            dateEndDate.IsEnabled = true;
+        }
+
+        private void TurnOffUI()
+        {
+            btnEdit.Visibility = Visibility.Visible;
+            btnSave.Visibility = Visibility.Hidden;
+
+            txtDestination.IsReadOnly = true;
+            cbCountry.IsEnabled = false;
+            txtTravellers.IsReadOnly = true;
+            cbReason.IsEnabled = false;
+            txtMeetingDetails.IsReadOnly = true;
+            xbAllInclusive.IsEnabled = false;
+            dateStartDate.IsEnabled = false;
+            dateEndDate.IsEnabled = false;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            string destination = txtDestination.Text;
+            Country country = (Country)cbCountry.SelectedItem;
+            int.TryParse(txtTravellers.Text, out int travellers);
+            DateTime startDate = (DateTime)dateStartDate.SelectedDate;
+            DateTime endDate = (DateTime)dateEndDate.SelectedDate;
+
+            if (destination == "" || travellers == 0 || country == null || startDate == null || endDate == null)
+            {
+                MessageBox.Show("Invalid info, check inputs", "Warning");
+                return;
+            }
+
+            try
+            {
+                if ((string)cbReason.SelectedItem == "Work trip")
+                {
+                    //TODO: Något måste göras om sortens resa ändras
+                    string meetingDetails = txtMeetingDetails.Text;
+                    WorkTrip updatedTravel = new(destination, country, travellers, new List<PackingListItem>(), TravelManager.SelectedTravel.User, startDate, endDate, meetingDetails);
+                    TravelManager.UpdateTravel(updatedTravel);
+                }
+                else if ((string)cbReason.SelectedItem == "Vacation")
+                {
+                    bool allInclusive = false;
+                    if (xbAllInclusive.IsChecked == true)
+                    {
+                        allInclusive = true;
+                    }
+                    Vacation updatedTravel = new(destination, country, travellers, new List<PackingListItem>(), TravelManager.SelectedTravel.User, startDate, endDate, allInclusive);
+                    TravelManager.UpdateTravel(updatedTravel);
+
+                }
+
+
+                MessageBox.Show("Travel updated!");
+
+                UpdateUI();
+                TurnOffUI();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}, try again", "Warning");
+            }
+
+        }
+
+        private void cbReason_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (cbReason.SelectedItem == "Work Trip")
+            {
+                lblMeetingDetails.Visibility = Visibility.Visible;
+                lblAllInclusive.Visibility = Visibility.Hidden;
+
+                txtMeetingDetails.Visibility = Visibility.Visible;
+                xbAllInclusive.Visibility = Visibility.Hidden;
+            }
+            else if (cbReason.SelectedItem == "Vacation")
+            {
+                lblMeetingDetails.Visibility = Visibility.Hidden;
+                lblAllInclusive.Visibility = Visibility.Visible;
+
+                txtMeetingDetails.Visibility = Visibility.Hidden;
+                xbAllInclusive.Visibility = Visibility.Visible;
+            }
         }
     }
 }
