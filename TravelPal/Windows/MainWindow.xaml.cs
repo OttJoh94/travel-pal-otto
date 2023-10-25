13e0cdeb21ef98d23db4d2b32faf2544b0f57344
OttJoh94 <145072357+OttJoh94@users.noreply.github.com>
2023-10-25 15:23:39 +0200

    add closebutton on MainWindow

--- TravelPal/Windows/MainWindow.xaml.cs
+++ TravelPal/Windows/MainWindow.xaml.cs
@@ -71,5 +71,13 @@ namespace TravelPal
                 DragMove();
             }
         }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
